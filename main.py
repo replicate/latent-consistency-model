66d87282a73991fed38da2aafa47ee702ea6281d
Jesse Andrews <anotherjesse@gmail.com>
2023-10-24 13:17:10 -0400

    prootype of img2img

--- main.py
+++ main.py
@@ -1,33 +1,46 @@
 import os
 import torch
 import argparse
+from cog import BasePredictor
 import time
 from diffusers import DiffusionPipeline
+from latent_consistency_img2img import LatentConsistencyModelImg2ImgPipeline
+from PIL import Image
 
-class Predictor:
-    def __init__(self):
-        self.pipe = self._load_model()
-
-    def _load_model(self):
-        model = DiffusionPipeline.from_pretrained(
+class Predictor(BasePredictor):
+    def setup(self):
+        self.txt2img = DiffusionPipeline.from_pretrained(
             "SimianLuo/LCM_Dreamshaper_v7",
             custom_pipeline="latent_consistency_txt2img",
             custom_revision="main"
         )
-        model.to(torch_device="cpu", torch_dtype=torch.float32).to('mps:0')
-        return model
+        self.txt2img.to(torch_device="cpu", torch_dtype=torch.float32).to('mps:0')
+        self.img2img = DiffusionPipeline.from_pretrained("SimianLuo/LCM_Dreamshaper_v7", custom_pipeline="latent_consistency_img2img.py").to('mps:0')
 
-    def predict(self, prompt: str, width: int, height: int, steps: int, seed: int = None) -> str:
+
+    def predict(self, prompt: str, width: int, height: int, steps: int, seed: int = None, image: str = None) -> str:
         seed = seed or int.from_bytes(os.urandom(2), "big")
         print(f"Using seed: {seed}")
         torch.manual_seed(seed)
 
-        result = self.pipe(
-            prompt=prompt, width=width, height=height,
-            guidance_scale=8.0, num_inference_steps=steps,
-            num_images_per_prompt=1, lcm_origin_steps=50,
-            output_type="pil"
-        ).images[0]
+        if image:   
+            print("img2img")
+            input_image=Image.open(image)
+            strength = 0.75
+            result = self.img2img(
+                image=input_image, prompt=prompt, width=width, height=height,
+                guidance_scale=8.0, num_inference_steps=steps, strength=strength,
+                num_images_per_prompt=1, lcm_origin_steps=50,
+                output_type="pil"
+            ).images[0]
+        else:
+            print("txt2img")
+            result = self.txt2img(
+                prompt=prompt, width=width, height=height,
+                guidance_scale=8.0, num_inference_steps=steps,
+                num_images_per_prompt=1, lcm_origin_steps=50,
+                output_type="pil"
+            ).images[0]
 
         return self._save_result(result)
 
@@ -40,12 +53,18 @@ class Predictor:
 def main():
     args = parse_args()
     predictor = Predictor()
+    predictor.setup()
 
     if args.continuous:
         try:
+            output_path = args.image
             while True:
-                output_path = predictor.predict(args.prompt, args.width, args.height, args.steps, args.seed)
+                output_path = predictor.predict(args.prompt, args.width, args.height, args.steps, args.seed, output_path)
                 print(f"Output image saved to: {output_path}")
+                f = open('index.html','w')
+                f.write('<img src="'+output_path+'">')
+                f.close()
+                
         except KeyboardInterrupt:
             print("\nStopped by user.")
     else:
@@ -59,7 +78,9 @@ def parse_args():
     parser.add_argument("--height", type=int, default=512, help="The height of the generated image.")
     parser.add_argument("--steps", type=int, default=8, help="The number of inference steps.")
     parser.add_argument("--seed", type=int, default=None, help="Seed for random number generation.")
+    parser.add_argument("--image", type=str, default=None, help="The path to the image to be used as a base.")
     parser.add_argument("--continuous", action='store_true', help="Enable continuous generation.")
+    parser.add_argument("--html", action='store_true', help="store html page.")
     return parser.parse_args()
 
 if __name__ == "__main__":
