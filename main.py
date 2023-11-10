31ddb78f94cca258cca80bddb41292e3078f7fb2
Robert Bolender <robert@bliteral.com>
2023-11-09 20:45:54 -0800

    Update diffusers package to 0.23.0
    LCM is now supported as a native pipeline and we no longer need
    to load a custom pipeline here.
    
    https://github.com/0xbitches/sd-webui-lcm/issues/26
    
    Fixes: https://github.com/replicate/latent-consistency-model/issues/8

--- main.py
+++ main.py
@@ -11,8 +11,6 @@ class Predictor:
     def _load_model(self):
         model = DiffusionPipeline.from_pretrained(
             "SimianLuo/LCM_Dreamshaper_v7",
-            custom_pipeline="latent_consistency_txt2img",
-            custom_revision="main"
         )
         model.to(torch_device="cpu", torch_dtype=torch.float32).to('mps:0')
         return model
